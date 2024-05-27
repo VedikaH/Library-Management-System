515ef8e003305b245a4b4b58a5aaeef53edde108
VedikaH <118132147+VedikaH@users.noreply.github.com>
2024-05-27 10:57:13 +0530

    Update user.py

--- user.py
+++ user.py
@@ -1,13 +1,15 @@
-""" users = []
-
-def add_user(name, user_id):
-    users.append({"name": name, "user_id": user_id}) """
 
 class User:
     def __init__(self, name, user_id):
+        """
+        Initialize a User object.
+
+        Args:
+            name (str): The name of the user.
+            user_id (str): The ID of the user.
+        """
         self.name = name
         self.user_id = user_id
 
     def __repr__(self):
         return f"User(name={self.name}, user_id={self.user_id})"
-
