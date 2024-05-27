cb4968a719b355b2be3c768dfe31b25d9d44411a
VedikaH <118132147+VedikaH@users.noreply.github.com>
2024-05-27 10:46:12 +0530

    Update book.py

--- book.py
+++ book.py
@@ -1,5 +1,15 @@
 class Book:
     def __init__(self, title, author, isbn, available=True):
+
+        """
+        Initialize a Book object with the provided title, author, ISBN, and availability.
+
+        Args:
+            title (str): The title of the book.
+            author (str): The author of the book.
+            isbn (str): The ISBN (International Standard Book Number) of the book.
+            available (bool, optional): The availability status of the book. Default is True.
+        """
         self.title = title
         self.author = author
         self.isbn = isbn
